14d5546639a17da91a4aa491031029fdcd517afe
dichouhan <dichouhan@deqode.com>
2023-01-30 13:51:44 +0530

    added ui for crud operations on admin console

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,13 @@ var mongo = require('mongodb');
 var db_url = "mongodb://127.0.0.1:27017/"
 var router = express();
 module.exports = router;
+var flash = require('connect-flash');
 
 
 var client = new mongo.MongoClient(db_url);
 var admin_db = client.db("restaurants").collection("admin")
+
+router.use(flash())
 
 
 
@@ -39,29 +42,56 @@ router.post("/admin/add_item", function(req, res, next){
     
     client.connect().then(()=>{
         admin_db.insertOne(doc).then((result)=>{
-            admin_db.find().then(function(value){
-                value.toArray().then(function(items){
-                    res.render("/admin/admin", {items: items});
-                })
-            })
+            req.flash("message", "added item!");
+            res.redirect("/admin");
         })
     })
 })
 
 
 router.post("/admin/add_item/:itemID", function(req, res, next){
-    let itemID = req.params["itemID"]
+    let itemID = req.params.itemID
+
     client.connect().then(()=>{
 
-        // admin_db.find().then(function(value){
-        //     value.toArray().then(function(items){
-        //         res.render("admin/admin", {items: items});
-        //     })
-        // })
+        const updateDoc = {
+            $set: {
+                name: req.body.name,
+                price: req.body.price,
+                image: req.body.image
+            },
+        };
 
-        // console.log(itemID);
+        const options = { upsert: true };
 
-        res.render("admin/admin", {items: items});
+        admin_db.updateOne({_id: itemID}, updateDoc, options).then((result)=>{
+            admin_db.find().toArray().then(function(items){
+                console.log(items);
+                res.render("admin/admin", {items: items});
+            })
+
+            console.log(itemID);
+            req.flash("update success!")
+            // res.render("admin/admin", {items: items});
+
+        })
+
     })
 
+})
+
+router.delete("/admin/delete", function(req, res, next){
+    let itemID = req.body.id
+    console.log(itemID);
+    client.connect().then(()=>{
+        let filter = {_id: mongo.ObjectId(itemID)};
+        console.log(filter);
+        
+        admin_db.findOneAndDelete(filter).then((result)=>{
+            console.log(result);
+
+            res.send("deleted successfuly!")
+            // res.redirect("/admin")
+        })
+    })
 })
